cc896d7ccebc629b43d714f90b51d32ed4ea7d21
Umutonipamera <umutonipamera@gmail.com>
2021-02-28 12:07:20 +0300

    Assignment2

---
+++ src/Assignment2.kt
@@ -0,0 +1,28 @@
+fun main () {
+    name()
+    var result= mod (5,10)
+    println (result)
+    var sum= add (10,30,20,50)
+    println (sum)
+    Fact()
+
+}
+fun name () {
+var name= "Pamera"
+    println ("Hello Pamera")
+}
+fun mod (d:Int,c:Int) :Int {
+    var result = d%c
+    return result
+}
+fun add (a:Int,b:Int,c:Int,d:Int):Int {
+    var sum= a+b+c+d
+    return  sum
+}
+fun Fact() {
+    var Int = "I love watching movie"
+    println (Int)
+}
+
+
+
